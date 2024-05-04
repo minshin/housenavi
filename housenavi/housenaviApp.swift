3accf2d270b8d9c877a36b708914c5fc61b203a7
minshin <tangmx@gmail.com>
2024-05-04 17:35:40 +0900

    Initial Commit

---
+++ housenavi/housenaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  housenaviApp.swift
+//  housenavi
+//
+//  Created by hiroshi on 2024/05/04.
+//
+
+import SwiftUI
+
+@main
+struct housenaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
