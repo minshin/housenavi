e1ef5162cf4789c626fe6a2d8f8eda655c7f6d2b
minshin <tangmx@gmail.com>
2024-05-20 07:30:22 +0900

    init

--- housenavi/housenaviApp.swift
+++ housenavi/housenaviApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import FirebaseCore
 import FirebaseAuth
 
+//如果您使用的是 SwiftUI，则必须创建应用委托并通过 UIApplicationDelegateAdaptor 或 NSApplicationDelegateAdaptor 将其附加到 App 结构体。您还必须停用应用委托调配。如需了解详情，请参阅 SwiftUI 说明。
+
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         print("Use Firebase library to configure APIs")
